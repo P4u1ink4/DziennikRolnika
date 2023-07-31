ae51cfe50a18de78a22c473f06dc118af5fdf4d9
Paulina Guzior <paulinkaguzior7@gmail.com>
2023-07-31 19:23:44 +0200

    creating main view

--- DziennikRolnika/DziennikRolnika/ContentView.swift
+++ DziennikRolnika/DziennikRolnika/ContentView.swift
@@ -9,13 +9,109 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack(alignment: .center) {
+                Text(currentDateFormatted())
+                    .font(.title)
+                Image("sunny")
+                VStack {
+                    Text("NAJBLIŻSZE NA POLU:")
+                        .font(.subheadline)
+                        .frame(width: 200.0)
+                    Text("- nie wiem jeszcze")
+                        .font(.footnote)
+                        .foregroundColor(Color(hue: 1.0, saturation: 0.013, brightness: 0.304))
+                        .frame(width: 200.0)
+                    Spacer()
+                        .frame(height: 20.0)
+                    Text("OPCJONALNE WYMAGANIA:")
+                        .font(.subheadline)
+                        .frame(width: 200.0)
+                    Text("- nie wiem jeszcze np. pogoda")
+                        .font(.footnote)
+                        .foregroundColor(Color(hue: 1.0, saturation: 0.013, brightness: 0.304))
+                        .frame(width: 200.0)
+                    }
+                    .padding()
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(Color(hue: 1.0, saturation: 0.013, brightness: 0.304), lineWidth: 2)
+                    )
+                Spacer()
+                    .frame(height: 20.0)
+                VStack {
+                    Text("DO ZROBIENIA:")
+                        .font(.subheadline)
+                        .frame(width: 200.0)
+                    Text(" - zrobić to do list")
+                        .frame(width: 200.0)
+                        .foregroundColor(Color(hue: 1.0, saturation: 0.013, brightness: 0.304))
+                        .font(.footnote)
+                    }
+                    .padding()
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 5)
+                            .stroke(Color(hue: 1.0, saturation: 0.013, brightness: 0.304), lineWidth: 2)
+                    )
+                Spacer()
+                    .frame(height: 20.0)
+                VStack(alignment: .center, spacing: 15.0){
+                    NavigationLink(destination: YearCalender()) {
+                        Text("Kalendarz roczny")
+                            .foregroundColor(Color(hue: 1.0, saturation: 0.013, brightness: 0.304))
+                    }
+                    NavigationLink(destination: Cows()) {
+                        Text("Spis krów")
+                            .foregroundColor(Color(hue: 1.0, saturation: 0.013, brightness: 0.304))
+                    }
+                    NavigationLink(destination: Fields()) {
+                        Text("Spis pól")
+                            .foregroundColor(Color(hue: 1.0, saturation: 0.013, brightness: 0.304))
+                    }
+                    }
+                    .padding()
+                Spacer()
+                    .frame(height: 100.0)
+
+            }
+        }
     }
+}
+
+func currentDateFormatted() -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "dd.MM.yyyy"
+    return dateFormatter.string(from: Date())
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+        }
+    }
+}
+
+struct YearCalender: View {
+    var body: some View {
+        VStack {
+        }
+        .navigationTitle("Kalendarz roczny")
+    }
+}
+
+struct Cows: View {
+    var body: some View {
+        VStack {
+        }
+        .navigationTitle("Spis krów")
+    }
+}
+
+struct Fields: View {
+    var body: some View {
+        VStack {
+        }
+        .navigationTitle("Spis pól")
     }
 }
