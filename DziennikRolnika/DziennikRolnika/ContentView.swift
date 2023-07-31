14c8cd2867ba7d0ee968f420138f99bcdda3f610
Paulina Guzior <paulinkaguzior7@gmail.com>
2023-07-31 17:26:23 +0200

    first time

---
+++ DziennikRolnika/DziennikRolnika/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DziennikRolnika
+//
+//  Created by Paulina Guzior on 31/07/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DziennikRolnika/DziennikRolnika/DziennikRolnikaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DziennikRolnikaApp.swift
+//  DziennikRolnika
+//
+//  Created by Paulina Guzior on 31/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct DziennikRolnikaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
